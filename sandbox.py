9b5c392b3a3e36f163188653e8d2e8975e79bb19
eugraczynski <eug.raczynski@gmail.com>
2025-11-28 12:58:43 +0100

    upd

--- sandbox.py
+++ sandbox.py
@@ -64,3 +64,14 @@ print(fake.currency_code())
 # <_>
 
 
+
+print("car" in ["car", "bus", "train"])
+#  test_ in pytest
+# immutable structure
+# error handling
+print([x * 2 for x in [1, 2, 3]])
+
+# aspice
+# (Python, CAN tools, automation logs)
+
+
