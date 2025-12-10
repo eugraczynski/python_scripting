75a37257f19117355e46e4d6ade302f3ec4cf503
eugraczynski <eug.raczynski@gmail.com>
2025-12-10 09:02:19 +0100

    up

--- sandbox/strings.py
+++ sandbox/strings.py
@@ -37,9 +37,9 @@ def method_3():
 
 # other (smarter) solutions
 
-print(string_[::-1])  # fastest and cleanest method
+# print(string_[::-1])  # fastest and cleanest method
 print(
-    "-".join(reversed(string_))
+    # "-".join(reversed(string_))
 )  # readable, also join will ITERATE throuhg whole string
 
 
@@ -51,12 +51,12 @@ def recursion_reverse(string_):
         return string_[-1] + recursion_reverse(string_[:-1])
 
 
-print(recursion_reverse(string_))
+# print(recursion_reverse(string_))
 
 # ?>>??????D??DAW?DA?WD?
 # why
 lsls = "0123456789"
-print(lsls[2::-2])
+# print(lsls[2::-2])
 
 
 # sum([int(x) ** len(str(string_)) for x in str(string_)])
@@ -72,3 +72,19 @@ print(lsls[2::-2])
 # .decode .encode 
 # for network package handling
 
+
+tests = [([1,2,3,4,5], [2,3,4,5]), ([5,4,1,3], [5,4,3]), ([1,2,1], [2,1])]
+
+def remove_smallest(list_):
+    newlist = list_.copy()
+    newlist.remove(min(list_))
+    return newlist
+
+def loop():
+    for test in tests:
+        print(test[0], test[1])    
+        result = remove_smallest(test[0])
+        assert result == test[1], "Wrong :("
+        assert result is not test[0], "You can't change original list"
+
+loop()
